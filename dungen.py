adc938bf1ddef9331626919999b672d1a4d0acb3
weston maine boldt <boldt.weston@gmail.com>
2019-11-03 22:54:33 -0600

    init commit

---
+++ dungen.py
@@ -0,0 +1,127 @@
+import math
+import random
+import sys
+from pprint import pprint 
+
+import itertools 
+
+#import pyglet 
+
+max_height = 5000
+max_width = 5000
+
+#size = width, height = 320, 240
+
+#black = 255, 255, 0
+#window = pyglet.window.Window()
+
+cells = 150
+
+class Room:
+    def __init__(self, position, width, height, maxWidth, maxHeight, grid):
+        self.position = position
+        self.x = position[0]
+        self.y = position[1]
+        self.width = width
+        self.height = height
+        self.neighbors = list()
+
+    @property
+    def left(self):
+        return self.x - (self.width/2)
+
+    @property
+    def right(self):
+        return self.x + (self.width/2)
+
+    @property
+    def top(self):
+        return self.y + (self.height/2)
+
+    @property
+    def bottom(self):
+        return self.y - (self.height/2)
+
+    def getDistanceBetweenRoom(self, room):
+        return math.sqrt((room.x - self.x) ** 2 + (room.y - self.y)**2)
+
+    def repulse(self, room):
+        dx = (self.x - room.x)
+        dy = (self.y - room.y)
+        self.x += dx + random.randint(-10, 10) * random.randint(-1, -1)
+        self.y += dy + random.randint(-10, 10) * random.randint(-1, -1)
+
+    def isRoomOverlap(self, room):
+        if (self.left > room.right or room.left > self.right):
+            return False
+        if (self.top < room.bottom or room.top < self.bottom):
+            return False
+        return True
+
+    def isPointInsideRoom(self, x, y, room):
+        if (x > room.left) and (x < room.right) and (y < room.top) and (y > room.bottom):
+            return True
+        return False
+
+class Dungeon:
+    def __init__(self, radius, numOfRooms, width=None, height=None, grid=None):
+        self.rooms = None
+        self.placeRooms(radius, numOfRooms)
+
+    def placeRooms(self, radius, numOfRooms):
+        rooms = []
+        for x in range(numOfRooms):
+            pos = getRandomPointInCircle(radius)
+            rooms.append(Room(pos, 32, 32, 16, 16, 4))
+        self.rooms = rooms
+
+        count = 0
+        done = False
+        while not done:
+            #while(collisions != 0):
+            count += 1
+            done = True
+            collisions = self.scatterRooms(self.rooms)
+            if collisions > 0:
+                done = False
+            print(collisions)
+
+        pprint(list(map(lambda room: (room.x, room.y), rooms)))
+        return rooms
+
+    def scatterRooms(self,rooms):
+        print("scattering")
+        collisions = 0
+        for i in rooms:
+            for j in rooms:
+                if i != j and j.isRoomOverlap(i):
+                    collisions += 1
+                    j.repulse(i)
+
+        if (collisions) == 0:
+            print('really?')
+
+        return collisions
+        
+
+def roundm(n, m): return math.floor(((n + m - 1) / m)) * m
+
+def getRandomPointInCircle(radius, tile_size=4):
+    t = 2 * math.pi * random.random()
+    u = random.random() + random.random()
+    r = 0
+    if u > 1:
+        r = 2 - u
+    else:
+        r = u
+    return roundm(radius * r * math.cos(t), tile_size), roundm(radius * r * math.sin(t), tile_size)
+        
+    
+def main():
+    numOfRooms = 20
+    radius = 80
+    
+    dungeon = Dungeon(radius, numOfRooms)
+
+if __name__ == '__main__':
+    main()
